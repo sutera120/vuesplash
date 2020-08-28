c525c52e9dc1053d4cd8c506b3b421464292c99d
sutera120 <sutera.works@gmail.com>
2020-08-28 12:31:05 +0900

    First commit

---
+++ web/resources/js/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="message" v-show="message">{{ message }}</div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      message: (state) => state.message.content,
+    }),
+  },
+};
+</script>
---
+++ web/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+
+// 会員登録
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+
+// ログイン
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+
+// ログアウト
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
+// ログインユーザー
+Route::get('/user', fn () => Auth::user())->name('user');
+
+// 写真投稿
+Route::post('/photos', 'PhotoController@create')->name('photo.create');
